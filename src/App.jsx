7b21802af800f429babc96e4d67a32dbaf2f15c4
kaifyyy <mkaifm728@gmail.com>
2025-10-05 01:19:41 +0530

    Three Fiber Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Canvas } from "@react-three/fiber";
-
+import "./style.css"
 
 const App = () => {
   return <Canvas>
     <mesh>
       <boxGeometry />
-        
+
     </mesh>
 
   </Canvas>
